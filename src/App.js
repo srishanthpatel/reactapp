6a3c8842c8c3303bcb991927bfc07bb59700235b
srishanthpatel <royalsrishanth9492@gmail.com>
2025-06-04 16:21:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { Component } from 'react';
+import {ThreeDots} from 'react-loader-spinner'
 
 class App extends Component{
 
-  state={list:[]}
+  state={list:[],isloading:true}
 
 componentDidMount(){
   this.getitems()
@@ -17,16 +18,18 @@ getitems= async ()=>{
     const response = await fetch(api,options)
     if(response.ok===true){
       const data = await response.json()
-      this.setState({list:data})
+      this.setState({list:data,isloading:false})
       console.log(data)
     }
     
 }
   render(){
-    const {list}=this.state
+    const {list,isloading}=this.state
     return(
       <div className='main'>
         <h1>Btech Trips</h1>
+        {isloading ? (<><div className='loader'>
+      <ThreeDots color="#ffffff" height="50" width="80" /></div></>):(<>
         <ul className='cont'>{
           list.map((eachitem)=>((
             <li className='li'>
@@ -39,7 +42,7 @@ getitems= async ()=>{
           )))}</ul>
           <div>
           <p className='pp'>Wanna Add More ? Click And Make Api Request</p><a href="https://apitesting-qcys.onrender.com/api/items">
-          <button className='btn'>Click</button></a></div>
+          <button className='btn'>Click</button></a></div></>)}
       </div>
     )
   }
